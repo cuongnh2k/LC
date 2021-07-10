8ceb1d7b77474f9cf66fda1855fe3062588eb316
cuongnh2k <cuongnh2k@gmail.com>
2021-07-10 20:13:28 +0700

    first commit

---
+++ LC/src/main/java/context/DBContext.java
@@ -0,0 +1,23 @@
+package context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBContext {
+
+	public Connection getConnection() {
+		final String url = "jdbc:mysql://localhost:3306/ban_hang";
+		final String user = "root";
+		final String password = "Huyenanh98";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
---
+++ LC/src/main/java/control/Test.java
@@ -0,0 +1,11 @@
+package control;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 300000000;
+		System.out.println(x);
+	}
+
+}
